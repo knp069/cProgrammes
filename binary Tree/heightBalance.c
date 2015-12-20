0263c832eb4cf7a013286cf0a0ea62d7b966e876
nishant <knp069@gmail.com>
2015-12-20 19:36:03 +0530

    update

--- binary Tree/heightBalance.c
+++ binary Tree/heightBalance.c
@@ -1,21 +1,53 @@
-#define and &&
-#define or ||
-#define null NULL
 #include <stdio.h>
 #include <stdlib.h>
+
+#define null NULL
+
 typedef struct node_tag{
     int data;
-    struct node_tag *left,*right;
+    struct node_tag *right,*left;
 }node;
 
 node* newNode(int data){
-    node* newnode = (node*) malloc(sizeof(node));
+    node* newnode = (node*)malloc(sizeof(node));
     newnode->data = data;
-    newnode->left = newnode->right = NULL;
+    newnode->left = newnode->right = null;
     return newnode;
 }
 
-int heightBalance(node* root){
-    
-    return heightBalance(root->right) && heightBalance(root->right);
+int max(int x , int y){
+    return x>y?x:y;
+}
+
+int height(node* root){
+
+    if(root == null){
+        return 0;
+    }
+
+    return max(height(root->left),height(root->right))+1;
+}
+
+int isHeightBalance(node* root){
+
+    if (root == null)
+        return 1;
+
+    int balanceFactor = abs( height(root->left) - height(root->right) ) < 2 ? 1 : 0 ;
+
+    return balanceFactor && isHeightBalance(root->left) && isHeightBalance(root->right);
+
+}
+
+int main(){
+
+    node* root = newNode(5);
+    root->right = newNode(4);
+    if (isHeightBalance(root)){
+        printf("tree is height balanced \n");
+    }
+    else{
+        printf("tree is not height balanced \n");
+    }
+    return 0;
 }
