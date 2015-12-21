dcbe9fe5c7e7c1d3a6a67cece322bbbca7dd363f
nishant <knp069@gmail.com>
2015-12-21 19:29:59 +0530

    sub-string comparsion

--- Strings/substring-comparision.c
+++ Strings/substring-comparision.c
@@ -15,6 +15,31 @@ int isSubstring(char* field , char* key){
 
 	int searchSpan = strlen(field)-strlen(key);
 
-	int i=0
-	while(i<=searchSpan)
+	int i=0,j,flag;
+	while(i<=searchSpan){
+		flag = 0;
+		j = 0;
+		while(key[j] != 0){
+			if(key[j] != field[i]){
+				flag = 1;
+			}
+			i++;
+			j++;
+		}
+		if (flag != 1)
+			break;
+	}
+	if (i > searchSpan && flag == 1)
+		return -1;
+
+	return i-j;
+}
+
+
+int main(){
+
+	char a[] = "pine-apple";
+	char b[] = "apple";
+	printf("Match found at index %d \n" , isSubstring(a,b));
+	return 0;
 }
