ea5300281bb8e0f5687acf99ab3a2c20b7c3df56
nishant <knp069@gmail.com>
2015-12-23 13:35:39 +0530

    stack-using-queue

---
+++ Linked List/stack-using-queue.c
@@ -0,0 +1,128 @@
+#define null NULL
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node_tag{
+	int data;
+	struct node_tag *next;
+}node;
+
+typedef struct queue_tag{
+    node* front;
+    node* rear;
+}queue;
+
+queue *tmpqu;
+
+int max(int x , int y){
+	return x>y?x:y;
+}
+
+node* newNode(int data){
+	node* newnode = (node*)malloc(sizeof(node));
+	newnode->data = data;
+	newnode->next = NULL;
+	return newnode;
+}
+
+void print(node* head){
+	node* ptr = head;
+
+	while(ptr!=null){
+		printf("%d\t",ptr->data);
+		ptr=ptr->next;
+	}
+	printf("\n");
+	return;
+}
+
+void initialise(queue **qu){
+    *qu = (queue*)malloc(sizeof(queue));
+    (*qu)->rear = (*qu)->front = null;
+}
+
+int isEmpty(queue* qu){
+    return (qu->rear == null && qu->front == null)?1:0;
+}
+
+int enqueue(queue* qu , int data){
+    node* temp = newNode(data);
+
+    if (temp == null)
+        return 0;
+
+	if (isEmpty(qu)){
+		temp->next = qu->rear;
+		qu->rear = qu->front = temp;
+		return 1;
+	}
+
+    temp->next = qu->rear;
+    qu->rear = temp;
+    return 1;
+}
+
+int dequeue(queue* qu){
+	node* ptr = qu->rear;;
+    node* prev = null;
+	int retval;
+
+	if (isEmpty(qu))
+        return -9999;
+
+	prev = null;
+
+	if( qu->front == qu->rear){
+		retval = qu->front->data;
+		free(qu->front);
+		qu->front = qu->rear = null;
+		return retval;
+	}
+
+	while(ptr->next != null){
+        prev = ptr;
+		ptr = ptr->next;
+	}
+	retval = ptr->data;
+    free(ptr);
+	qu->front = prev;
+    prev->next = NULL;
+    return retval;
+}
+
+int push(queue* qu , int data){
+    return enqueue(qu,data);
+}
+
+int pull(queue* qu ){
+
+    while(qu->rear != qu->front){
+        enqueue(tmpqu,dequeue(qu));
+    }
+    int retval = dequeue(qu);
+
+    while(!isEmpty(tmpqu)){
+        enqueue(qu,dequeue(tmpqu));
+    }
+
+    return retval;
+}
+
+int main(){
+    queue* qu;
+    initialise(&qu);
+    initialise(&tmpqu);
+    printf("%d\n",isEmpty(qu));
+    printf("%d\n",pull(qu));
+    printf("%d\n",push(qu,5));
+	printf("%d\n",pull(qu));
+	printf("%d\n",push(qu,5));
+    printf("%d\n",push(qu,2));
+    printf("%d\n",push(qu,3));
+    printf("%d\n",push(qu,4));
+    print(qu->rear);
+    printf("%d\n",isEmpty(qu));
+    printf("%d\n",pull(qu));
+    print(qu->rear);
+    return 0;
+}
